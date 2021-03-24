e441dc5fb71193c5f8878f606493f5e0efcb947e
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-24 10:28:40 -0700

    added specific messages and % readouts

--- app.js
+++ app.js
@@ -32,9 +32,27 @@ button.addEventListener('click', () => {
     if (!isYes(secondAnswer)) score++;
     if (isYes(thirdAnswer)) score++;
     // display results
-    const resultsReadout = `${name}, you scored ${score} out of 3!`;
+
     alert(`${name}, the quiz is complete! Your results will be displayed on the website.`);
 
+
+    //if statement for different result messages
+    if(score <= 1) {
+        score = Math.round((score / 3) * 100);
+        const resultsReadout = `${name}, you know Nick ${score}%.`;
+        results.textContent = resultsReadout
+    } else if(score === 2) {
+        score = Math.round((score / 3) * 100);
+        const resultsReadout = `${name}, you know Nick ${score}%. That's awesome!`;
+        results.textContent = resultsReadout
+    } else {
+        score = Math.round((score / 3) * 100);
+        const resultsReadout = `${name}, you know Nick ${score}%. Don't be such a stalker!`;
+        results.textContent = resultsReadout
+    }
+    //const resultsReadout = `${name}, you scored ${score} out of 3!`;
+    //alert(`${name}, the quiz is complete! Your results will be displayed on the website.`);
+
     // how? 'textContent = results'
-    results.textContent = resultsReadout
+    //results.textContent = resultsReadout
 });
