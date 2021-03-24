24f64d8d90fe3a5c9a48f3ffbdbc259567207b6c
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-23 18:40:12 -0700

    quiz created in app.js

--- app.js
+++ app.js
@@ -8,6 +8,31 @@ const results = document.getElementById('results');
 
 // set event listeners to update state and DOM
 button.addEventListener('click', () => {
-    //inform user they are in a quiz
+    // inform user they are in a quiz
+    alert('Welcome to the quiz!');
+    // check if they definitely want to take the quiz
+    const takeQuizYes = confirm('Would you like to proceed to the quiz?');
+
+    if (!takeQuizYes) {
+        alert('Okay, Goodbye!');
+        return;
+    }
+
+    // get name with 'prompt()'
+    const name = prompt ("What is your name?");
     
+    let score = 0;
+    // ask questions
+    const firstAnswer = prompt ("Does Nick write?");
+    const secondAnswer = promt ("Has Nick lived in 3 different states?")
+    const thirdAnswer = promt ("Dose Nick like the outdoors?")
+
+    //score if (isYes()) score ++
+    if (isYes(firstAnswer)) score++
+    if (!isYes(secondAnswer)) score++
+    if (isYes(thirdAnswer)) score++
+    // display results
+    const results = `${name}, you scored ${score} out of 3!`
+    alert(`${name}, the quiz is complete! Your results will be displayed on the website.`);
+    // how? 'textContent = results'
 })
